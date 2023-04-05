1d2b02f1355f739c8fd03b58bd4cef6709860825
Den Titkov <d.g.titkov@gmail.com>
2023-04-05 23:04:03 +0300

    corrected_.php file

--- web4/index.php
+++ web4/index.php
@@ -1,23 +1,8 @@
 <?php
 $skills = [
-    [
-        'name' => 'Front End Developer'
-        'dateStart' => 'January 2020'
-        'dateCompletion' => 'Current'
-        'description' => 'A Front-End Developer is responsible for developing new user-facing features, determining the structure and design of web pages, building reusable codes, optimizing page loading times, and using a variety of markup languages to create the web pages.'
-    ],
-    [
-        'name' => 'Web Developer / something.com'
-        'dateStart' => 'January 2015'
-        'dateCompletion' => 'January 2020'
-        'description' => 'A Web Developer is a professional who is responsible for the design and construction of websites. They ensure that sites meet user expectations by ensuring they look good, run smoothly and offer easy access points with no loading issues between pages or error messages.'
-    ],
-    [
-        'name' => 'Graphic Designer / designsomething.com'
-        'dateStart' => 'January 2010'
-        'dateCompletion' => 'January 2015'
-        'description' => 'A Graphic Designer is an artist who creates visual text and imagery. They design creative content for online campaigns, print ads, websites, and even videos..'
-    ]
+        'name' => ['Front-End Developer','Web Developer / something.com', 'Graphic Designer / designsomething.com'],
+        'date' => ['January 2020-Current', 'January 2015-January 2020', 'January 2010-January 2015'],
+        'description' => ['responsible for developing new user-facing features, determining the structure and design of web pages, building reusable codes, optimizing page loading times, and using a variety of markup languages to create the web pages.', 'A Web Developer is a professional who is responsible for the design and construction of websites. They ensure that sites meet user expectations by ensuring they look good, run smoothly and offer easy access points with no loading issues between pages or error messages.', 'A Graphic Designer is an artist who creates visual text and imagery. They design creative content for online campaigns, print ads, websites, and even videos..'],
 ]
 ?>
 <!DOCTYPE html>
@@ -114,15 +99,26 @@ $skills = [
           <h2 class="w3-text-grey w3-padding-16"><i
               class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
           <div class="w3-container">
-            <h5 class="w3-opacity"><b><?php echo $skills[$i]['name']; ?> </b></h5>
-            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $skills[$i]['dateStart']; ?> - <span
-                class="w3-tag w3-teal w3-round"><?php echo $skills[$i]['dateCompletion']; ?></span></h6>
-            <p><?php echo $skills[$i]['description']; ?></p>
+            <h5 class="w3-opacity"><b><?php echo $skills['name'][0]; ?></b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $skills['date'][0]; ?></h6>
+            <p><?php echo $skills['description'][0]; ?></p>
+            <hr>
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b><?php echo $skills['name'][1]; ?></b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $skills['date'][1]; ?></h6>
+            <p><?php echo $skills['description'][1]; ?></p>
+            <hr>
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b><?php echo $skills['name'][2]; ?></b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $skills['date'][2]; ?></h6>
+            <p><?php echo $skills['description'][2]; ?></p>
             <hr>
           </div>
         </div>
- 
-        <div class="w3-container w3-card w3-white">
+
+          <div class="w3-container w3-card w3-white">
           <h2 class="w3-text-grey w3-padding-16"><i
               class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
           <div class="w3-container">
